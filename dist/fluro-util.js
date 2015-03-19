a84ad6e58a2de26f7e564d83bdc673a734799f2a
Cade <cade@hyperblaster.org>
2015-03-19 15:44:42 +1100

    Added DateTools

--- dist/fluro-util.js
+++ dist/fluro-util.js
@@ -378,6 +378,68 @@ angular.module('fluro.util')
     return controller;
 
 });
+
+'use strict';
+
+
+angular.module('fluro.util')
+
+
+.factory('DateTools', function() {
+
+    var controller = {};
+
+    ///////////////////////////////////////
+
+    controller.readableDateRange = function(startDate, endDate) {
+
+
+        if(!_.isDate(startDate)) {
+            startDate = new Date(startDate);
+        }
+
+        if(!_.isDate(endDate)) {
+            endDate = new Date(endDate);
+        }
+
+
+
+        var today = new Date();
+        var string = '';
+
+        //We have a range
+        if (startDate.format('d/m/y') != endDate.format('d/m/y')) {
+            if (startDate.format('M Y') == endDate.format('M Y')) {
+                string = startDate.format('j') + ' - ' + endDate.format('j F');
+            } else {
+                string = startDate.format('l j F') + ' until ' + endDate.format('l j F');
+            }
+
+            //Append the year if the year is different from now
+            if (today.format('Y') != endDate.format('Y')) {
+                string = string + ' ' + endDate.format('Y');
+            }
+        } else {
+            if (startDate) {
+                string = startDate.format('l j F');
+
+                //Append the year if the year is different from now
+                if (today.format('Y') != startDate.format('Y')) {
+                    string = string + ' ' + startDate.format('Y');
+                }
+            }
+        }
+
+
+
+        return string;
+    }
+
+    ///////////////////////////////////////
+
+    return controller;
+
+});
 'use strict';
 
 
