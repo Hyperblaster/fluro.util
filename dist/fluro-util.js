1103682bb19cd6761cfb5eb8a898d20df64c11cd
Cade <cade@hyperblaster.org>
2015-07-16 18:31:33 +1000

    Added plain text filter

--- dist/fluro-util.js
+++ dist/fluro-util.js
@@ -90,6 +90,22 @@ angular.module('fluro.util', [
 
 }).call(this);
 
+angular.module('fluro.util')
+.filter('formatDate', function(){
+  return function(dateString, format){
+    return new Date(dateString).format(format)
+  };
+})
+angular.module('fluro.util')
+.filter('plaintext', function() {
+    return function(text) {
+        if (text) {
+            return String(text).replace(/<[^>]+>/gm, '');
+        } else {
+            return text;
+        }
+    };
+});
 'use strict';
 
 
