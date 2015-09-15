c4cc9492c17e70d76f89cc280cea7a7ca8e09efa
Cade <cade@hyperblaster.org>
2015-09-15 13:40:35 +1000

    Capitalize name filter

--- dist/fluro-util.js
+++ dist/fluro-util.js
@@ -91,6 +91,21 @@ angular.module('fluro.util', [
 
 }).call(this);
 
+angular.module('fluro.util').filter('capitalizename', function() {
+        return function(text) {
+            if (text) {
+                return text.replace(/\w\S*/g, function(txt){
+
+                    console.log('REPLACE',txt);
+                    return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+                });
+            } else {
+                return text;
+            }
+        };
+    });
+
+
 
 angular.module('fluro.util').filter('divide', function() {
   
